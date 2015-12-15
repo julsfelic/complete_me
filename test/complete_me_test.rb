1b2b9a673f3c7136c274151ce5f99d6c2fa4d0b3
Julian Feliciano <julsfelic@gmail.com>
2015-12-14 20:44:39 -0700

    Make sure root node has a value of an empty string

--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -14,4 +14,8 @@ class CompleteMeTest < Minitest::Test
   def test_has_a_root_that_points_to_a_node
     assert_instance_of Node, @completion.root
   end
+
+  def test_root_node_has_a_value_of_a_empty_string
+    assert_equal "", @completion.root.value
+  end
 end
